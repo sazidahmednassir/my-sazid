4111ad10a80deccd23d374023e345d5e242e302c
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-19 03:34:45 -0800

    FOUR commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import './App.css';
 import Allproducts from './Components/Allproducts/Allproducts';
 import Menubar from './Components/Menubar/Menubar';
 
 function App() {
+  const [count, setCount]=useState(0)
   return (
     <div className="App">
-     <Menubar></Menubar>
+     <Menubar count={setCount(count)}></Menubar>
      <Allproducts></Allproducts>
     </div>
   );
--- src/Components/Singleproduct/Singleproduct.js
+++ src/Components/Singleproduct/Singleproduct.js
@@ -7,7 +7,7 @@ const Singleproduct = (props) => {
         <div className='col-md-4'>
            
           <div className="card p-3 border">
-          <img className="w-50 m-auto" src={props.product.image} alt="" />
+          <img className="w-50 m-auto " src={props.product.image} alt="" />
           <h1>{props.product.title}</h1>
           <div className=" d-flex justify-content-around">
           <button className="btn btn-success">Add to Cart</button>
